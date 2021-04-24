08f49faa29c55e93cb9478f2c9f3e6cf1bbd2f36
nopeitagain@mail.ru <nopeitagain@mail.ru>
2021-04-24 16:44:46 +0300

    Начало начала.

--- CubeThrow/CubeThrow.cpp
+++ CubeThrow/CubeThrow.cpp
@@ -4,9 +4,37 @@
 
 using namespace std;
 
+int turn = 0, PlayerIsFirst = 0;
+
+void Queue()
+{
+	srand(time(NULL));
+	turn = rand() % 2;
+	if (turn == 1)
+	{
+		PlayerIsFirst = 1;
+		cout << "Вы ходите первый!" << endl;
+	}
+	if (turn = 0)
+	{
+		PlayerIsFirst = 0;
+		cout << "Компьютер ходит первый!" << endl;
+	}
+}
+
+int PCPoint = 0, PlayerPoints = 0, PCGapDrop = 0, PlayerGapDrop = 0;
+
+void FirstCubeThrow()
+{
+
+}
+void SecondCubeThrow()
+{
+
+}
+
 int main()
 {
 
-cout << "Hello World!\n";
 
 }
